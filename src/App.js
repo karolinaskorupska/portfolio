9bc7af2599dfabb42cd6360414ef6f2de8d0883a
karolinaskorupska <pixie22@op.pl>
2020-07-09 21:49:05 +0200

    components structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import ReactDOM from 'react-dom';
+ 
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+ 
+import App from './components/App';
+ 
+ReactDOM.render(<App />, document.getElementById('root'));
+ 
+serviceWorker.unregister();
---
+++ src/components/App/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Landing from "../Landing";
+
+const App = () => (
+    <div className="container">
+        <Landing/>
+    </div>
+);
+
+export default App;
+
